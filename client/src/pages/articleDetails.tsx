bd95e6aba30a1d2d551897bb58636a665b551518
Artas47 <artasss47@gmail.com>
2021-01-09 01:36:01 +0100

    showing popper on click

--- client/src/pages/articleDetails.tsx
+++ client/src/pages/articleDetails.tsx
@@ -1,4 +1,4 @@
-import { makeStyles } from '@material-ui/core';
+import { Box, Button, ClickAwayListener, Fade, makeStyles, Popper } from '@material-ui/core';
 import React from 'react'
 import { useParams } from 'react-router-dom';
 
@@ -6,36 +6,75 @@ const useStyles = makeStyles({
     word: {
         cursor: 'pointer',
         transition: 'all .2s',
+        width: '100%',
+        height: '1rem',
+        fontWeight: 300,
         '&:hover': {
-            // backgroundColor: 'red',
             transform: 'scale(1.5)',
-            color: '#70c1b3'
-        }
-    }
+            color: '#70c1b3',
+        },
+    },
+    popperBox: {
+        backgroundColor: '#fff',
+        display: 'flex',
+    },
 });
 
 interface ParamTypes {
     articleId: string
 }
 
-
-
 const e = 'siema elo czesc a elo ha ha halo monitor komputer ksiazka grzejnik'.split(' ');
-console.log('e', e)
 
 const ArticleDetails = () => {
     const classes = useStyles();
     const {articleId} = useParams<ParamTypes>();
-    return (
-        <div style={{margin: '2rem', fontSize: '3rem', textAlign: 'center'}}>
-            <img src='https://picsum.photos/200/300' />
-            <p style={{color: '#25554d', wordSpacing: '1rem', margin: '0 4rem'}}>
-                {e.map(q => {
-                    return <p className={classes.word} style={{display: 'inline'}} onClick={() => console.log(q)}> {q + ' '} </p>
-                })}
-            </p>
 
-        </div>
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [open, setOpen] = React.useState(false);
+
+    const id = open ? 'legend-popover' : undefined;
+
+    function handleClose() {
+        setOpen(false);
+    }
+
+    const handleClick = () => (event: any) => {
+        setAnchorEl(event.currentTarget);
+        setOpen(true);
+    };
+
+    return (
+        <ClickAwayListener onClickAway={handleClose}>
+            <div style={{margin: '2rem', fontSize: '3rem', textAlign: 'center'}}>
+                <img src='https://picsum.photos/200/300' />
+                <Popper
+                    id={id}
+                    open={open}
+                    anchorEl={anchorEl}
+                    transition
+                >
+                    {({ TransitionProps }) => (
+                        <Fade {...TransitionProps} timeout={350}>
+                            <Box className={classes.popperBox} boxShadow={1}>
+                                <Button style={{fontSize: '1.3rem', fontWeight: 400}}>
+                                    Check meaning
+                                </Button>
+                                <Button style={{fontSize: '1.3rem', fontWeight: 400}}>
+                                    Check synonyms
+                                </Button>
+                            </Box>
+                        </Fade>
+                    )}
+                </Popper>
+                <p style={{color: '#25554d', wordSpacing: '1rem', margin: '0 4rem'}}>
+                    {e.map(q => {
+                        return <> <p className={classes.word} style={{display: 'inline'}} onClick={handleClick()}>{q}</p> </>
+                    })}
+                </p>
+
+            </div>
+        </ClickAwayListener>
     )
 }
 
