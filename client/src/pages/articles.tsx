79b922865a473ae85b10c8b3c648bb6caf27577a
ARTAS <artasss47@gmail.com>
2021-01-23 01:43:57 +0100

    rendering articles from db

--- client/src/pages/articles.tsx
+++ client/src/pages/articles.tsx
@@ -3,6 +3,7 @@ import React, { useEffect } from "react";
 import ArticleItem from "../components/articleItem/articleItem";
 import { useDispatch, useSelector } from "react-redux";
 import { checkIfLoading } from "../redux/selectors/loadingSelector";
+import { articlesSelector } from "../redux/selectors/articlesSelector";
 import Fade from "@material-ui/core/Fade";
 import { FETCH_ARTICLES } from "../redux/types";
 
@@ -19,10 +20,10 @@ const Articles = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch({ type: FETCH_ARTICLES });
-  }, []);
+  }, [dispatch]);
+  const articles = useSelector(articlesSelector);
   const isLoading = useSelector(checkIfLoading(FETCH_ARTICLES));
   const classes = useStyles();
-  console.log("isLoading", isLoading);
   if (isLoading) {
     return (
       <div
@@ -34,51 +35,15 @@ const Articles = () => {
   return (
     <Fade in={!isLoading}>
       <Box className={classes.articlesWrapper}>
-        <ArticleItem
-          id="1"
-          title="simple title 123 123 i am a very simple title"
-          image="https://images.unsplash.com/photo-1576872381149-7847515ce5d8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=676&q=80"
-        />
-        <ArticleItem
-          id="2"
-          title="simple title 123 123 i afasdfa sdfam a very simple title"
-          image="https://images.unsplash.com/photo-1576872381149-7847515ce5d8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=676&q=80"
-        />
-        <ArticleItem
-          id="13"
-          title="simple title 123 123 i am a very simple title"
-          image="https://images.unsplash.com/photo-1576872381149-7847515ce5d8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=676&q=80"
-        />
-        <ArticleItem
-          id="14"
-          title="simple title 123 12fsdfas fasdfasdfsa dfasdfasd3 i am a very simple title"
-          image="https://images.unsplash.com/photo-1544716278-ca5e3f4abd8c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8Ym9va3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-        />
-        <ArticleItem
-          id="15"
-          title="simple title 123 123 i am a very simple title"
-          image="https://picsum.photos/200/300"
-        />
-        <ArticleItem
-          id="16"
-          title="simple title 123 123 i asdfs fsdf afasddf sASDF ASDFDF ASADFSA DFASDFm a very simple title"
-          image="https://picsum.photos/200/300"
-        />
-        <ArticleItem
-          id="16"
-          title="simple title 123 123 isdfasdfa sd am a very simple title"
-          image="https://picsum.photos/200/300"
-        />
-        <ArticleItem
-          id="71"
-          title="simple title 123 123 i am a very simple title"
-          image="https://picsum.photos/200/300"
-        />
-        <ArticleItem
-          id="18"
-          title="simple title 123 123 ifasdfa sdfasdf am a very simple title"
-          image="https://picsum.photos/200/300"
-        />
+        {articles.map((article: any) => {
+          return (
+            <ArticleItem
+              id={article._id}
+              title={article.title}
+              image="https://images.unsplash.com/photo-1576872381149-7847515ce5d8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=676&q=80"
+            />
+          );
+        })}
       </Box>
     </Fade>
   );
