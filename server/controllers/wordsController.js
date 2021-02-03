e88abe5534079963bbf782d77aa5fd33509e6c88
Artas47 <artasss47@gmail.com>
2021-02-03 23:44:23 +0100

    trying out linguee api

--- server/controllers/wordsController.js
+++ server/controllers/wordsController.js
@@ -1,15 +1,19 @@
 const Reverso = require("reverso-api");
+const linguee = require('linguee');
 
 const getWord = async (req, res, next) => {
   try {
-    const reverso = new Reverso();
-    const response = await reverso.getContext(
-      req.params.wordToTranslate,
-      "English",
-      "Polish"
-    );
+      linguee
+    .translate('money', { from: 'eng', to: 'pol' })
+    .then(function(response) {
+      console.log(response);
+    })
+    .catch(function(error) {
+      // ...
+    });
     res.send({ wordMeaning: response });
   } catch (err) {
+    console.log('err', err)
     return next(new Error("Could not fetch word, try again later"));
   }
 };
