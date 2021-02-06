db8236944079db4beab5f6b141ced33a4cfe01c4
Artas47 <artasss47@gmail.com>
2021-02-06 23:53:30 +0100

    sending back translated word

--- server/controllers/wordsController.js
+++ server/controllers/wordsController.js
@@ -1,17 +1,9 @@
 const linguee = require('linguee');
 
 const getWord = async (req, res, next) => {
-  console.log('req.params', req.params)
   try {
-      linguee
-    .translate(req.params.wordToTranslate, { from: 'eng', to: 'pol' })
-    .then(function(response) {
-      console.log(response);
-    })
-    .catch(function(error) {
-      // ...
-    });
-    // res.send({ wordMeaning: response });
+    const response = await linguee.translate(req.params.wordToTranslate, { from: 'eng', to: 'pol' });
+    res.send({ wordTranslated: response });
   } catch (err) {
     console.log('err', err)
     return next(new Error("Could not fetch word, try again later"));
