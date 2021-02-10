5dd5ee057adcfae1a3a63fff723c19345731869f
ARTAS <artasss47@gmail.com>
2021-02-10 23:38:24 +0100

    simple fix

--- client/src/redux/reducers/wordsReducer.js
+++ client/src/redux/reducers/wordsReducer.js
@@ -7,7 +7,7 @@ const initialState = {
 export default function articlesReducer(state = initialState, action) {
   switch (action.type) {
     case FETCH_WORD_SUCCESS:
-      return { ...state, word: action.payload };
+      return { ...state, word: action.payload.wordTranslated };
     default:
       return { ...state };
   }
--- server/controllers/wordsController.js
+++ server/controllers/wordsController.js
@@ -1,11 +1,14 @@
-const linguee = require('linguee');
+const linguee = require("linguee");
 
 const getWord = async (req, res, next) => {
   try {
-    const response = await linguee.translate(req.params.wordToTranslate, { from: 'eng', to: 'pol' });
+    const response = await linguee.translate(req.params.wordToTranslate, {
+      from: "eng",
+      to: "pol",
+    });
     res.send({ wordTranslated: response });
   } catch (err) {
-    console.log('err', err)
+    console.log("err", err);
     return next(new Error("Could not fetch word, try again later"));
   }
 };
